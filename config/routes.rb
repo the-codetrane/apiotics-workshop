bb5ccf8f39f98561bdf31ab2f38a5fd5f5551465
Michael Cain <michaelmcain@yahoo.com>
2018-04-19 14:38:37 -0400

    apiotics api init

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+		scope module: :security_device do 
+				resources :security_devices
+			end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
